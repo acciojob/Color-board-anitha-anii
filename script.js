cf279b33863f7a828e84f7809bd5590f6e0adeee
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-20 23:36:57 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.getElementById('container');
+const squares = 800;
+
+for (let i = 0; i < squares; i++) {
+  const innerDiv = document.createElement('div');
+  innerDiv.className = "square";
+  container.appendChild(innerDiv);
+}
+
