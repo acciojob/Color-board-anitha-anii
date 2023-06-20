c49f44c33d8e6b759b5f0296e71b67e0d316c1fd
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-20 23:59:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ for (let i = 0; i < squares; i++) {
   const innerDiv = document.createElement('div');
   innerDiv.className = "square";
   container.appendChild(innerDiv);
-}
 
+  innerDiv.addEventListener('mouseover', () => {
+    innerDiv.style.backgroundColor = "rgb(29, 29, 29)";
+    setTimeout(() => {
+      innerDiv.style.backgroundColor = "";
+    }, 1000);
+  });
+}
